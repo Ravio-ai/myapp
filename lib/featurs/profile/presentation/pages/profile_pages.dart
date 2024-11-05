323d61ef2307591ee1c5e111124e94e6479916c5
rioten <rioten353@gmail.com>
2024-11-05 10:37:19 +0000

    profile initial setup

--- lib/featurs/profile/presentation/pages/profile_pages.dart
+++ lib/featurs/profile/presentation/pages/profile_pages.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:myapp/featurs/auth/domain/entities/app_user.dart';
 import 'package:myapp/featurs/auth/presentation/cubits/auth_cubit.dart';
+import 'package:myapp/featurs/profile/presentation/cubits/profile_cubit.dart';
 
 class ProfilePage extends StatefulWidget {
   final String uuid;
@@ -13,13 +14,23 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   late final authCubit = context.read<AuthCubit>();
+  late final profileCubit = context.read<ProfileCubit>();
 
   late AppUser? user = authCubit.currentUser;
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      profileCubit.getProfileUser(widget.uuid);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(user!.email),
+        foregroundColor: Theme.of(context).colorScheme.primary,
       ),
     );
   }
