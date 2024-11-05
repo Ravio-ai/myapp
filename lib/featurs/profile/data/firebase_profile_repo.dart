2fbf0b5a0c0019cb9423495544c1e183d4859120
rioten <rioten353@gmail.com>
2024-11-05 09:28:24 +0000

    user firebase class completed

---
+++ lib/featurs/profile/data/firebase_profile_repo.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:myapp/featurs/profile/domain/entities/profile_user.dart';
+import 'package:myapp/featurs/profile/domain/repos/profile_repo.dart';
+
+class FirebaseProfileRepo implements ProfileRepo {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  @override
+  Future<ProfileUser?> getProfileUser(String uuid) async {
+    try {
+      final userDoc = await _firestore.collection('users').doc(uuid).get();
+      if (userDoc.exists) {
+        final userData = userDoc.data();
+        if (userData != null) {
+          return ProfileUser(
+            uuid: uuid,
+            name: userData['name'],
+            email: userData['email'],
+            bio: userData['bio'],
+            profileImageUrl: userData['profileImageUrl'],
+          );
+        }
+      }
+      return null;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  @override
+  Future<void> updateProfileUser(ProfileUser user) async {
+    try {
+      await _firestore.collection('users').doc(user.uuid).update({
+        'bio': user.bio,
+        'profileImageUrl': user.profileImageUrl,
+      });
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
